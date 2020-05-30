2e4a03349928e1f0f12295d1dd8c4879db75186d
5 <gerardoemery@live.com.mx>
2020-05-30 02:55:15 -0500

    form login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { AngularMaterialModule } from './material.module';
 
 import { AppComponent } from './app.component';
@@ -18,7 +20,10 @@ import { HeaderComponent } from './core/layout/header/header.component';
     BrowserModule,
     BrowserAnimationsModule,
     AngularMaterialModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/core/auth/login/login.component.ts
+++ src/app/core/auth/login/login.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, Validators } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  autForm: FormGroup;
+  switch = false;
+  constructor(
+    private formBuilder: FormBuilder,
 
-  constructor() { }
+  ) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.setForm()
+  }
+  setForm() {
+    this.autForm = this.formBuilder.group({
+      email: ['', Validators.required]
+    })
   }
 
+  onSubmit() {
+    console.log('form', this.autForm.value)
+  }
 }
